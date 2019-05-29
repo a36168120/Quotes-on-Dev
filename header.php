d831418d9b1eed04c8e65b4ad842e96a39e44e23
Vincent Wang <a36168120@gmail.com>
2019-05-29 16:48:09 -0700

    set up home page, add home & header in scss

--- header.php
+++ header.php
@@ -12,7 +12,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<?php wp_head(); ?>
 	</head>
 
@@ -23,12 +24,17 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
+
+				<div class="site-logo">
+					<a href="http://localhost:8888/Quotes-on-Dev/">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/qod-logo.svg" alt="##">
+					</a>
+				</div>
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<!-- took out nav php -->
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
